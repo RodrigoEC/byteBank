511142a978e7c4a1cef8c9cd20a45367c31102c0
rodrigoec <rodrigo.cavalcanti@ccc.ufcg.edu.br>
2021-03-19 22:19:26 -0300

    Refatoração da autenticação

--- src/SistemaAutenticacao.js
+++ src/SistemaAutenticacao.js
@@ -1,5 +1,13 @@
 export default class SistemaAutenticacao {
 	static login(autenticavel, senha) {
+		if (!SistemaAutenticacao.ehAutenticavel(autenticavel)) {
+			return false
+		}
 		return autenticavel.autenticaSenha(senha)
+	}
+
+	static ehAutenticavel(autenticavel) {
+		return 'autenticaSenha' in autenticavel &&
+			autenticavel.autenticaSenha instanceof Function
 	}
 }
